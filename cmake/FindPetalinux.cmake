2368619367f80c08140c2ba4f25614003434c841
Peter Kazanzides <pkaz@.jhu.edu>
2023-05-28 00:23:14 -0400

    Added version detection to FindPetalinux

--- cmake/FindPetalinux.cmake
+++ cmake/FindPetalinux.cmake
@@ -7,6 +7,17 @@
 if (DEFINED ENV{PETALINUX})
 
   message (STATUS "Found Petalinux in $ENV{PETALINUX}")
+
+  set (Petalinux_VERSION       $ENV{PETALINUX_VER})
+  string (FIND ${Petalinux_VERSION} "." DOT_POS)
+  if (DOT_POS EQUAL 4)
+    string (SUBSTRING ${Petalinux_VERSION} 0  4 Petalinux_VERSION_MAJOR)
+    string (SUBSTRING ${Petalinux_VERSION} 5 -1 Petalinux_VERSION_MINOR)
+    message (STATUS "Found Petalinux ${Petalinux_VERSION} (${Petalinux_VERSION_MAJOR}, ${Petalinux_VERSION_MINOR})")
+  else ()
+    message (WARNING "FindPetalinux: invalid version string ${Petalinux_VERSION}")
+  endif ()
+
   set (PETALINUX_USE_FILE "${CMAKE_SOURCE_DIR}/cmake/UsePetalinux.cmake")
   set (Petalinux_FOUND TRUE)
 
