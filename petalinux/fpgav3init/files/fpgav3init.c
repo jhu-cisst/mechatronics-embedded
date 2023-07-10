305660e05680ba4d03ee2c4d1ab68a47bed1ca95
Peter Kazanzides <pkaz@.jhu.edu>
2023-07-10 02:50:44 -0400

    Use EMIO to access FPGA write bus

--- petalinux/fpgav3init/files/fpgav3init.c
+++ petalinux/fpgav3init/files/fpgav3init.c
@@ -353,16 +353,22 @@ struct EMIO_Info {
     struct gpiod_chip *chip;
     struct gpiod_line_bulk reg_data_lines;
     struct gpiod_line_bulk reg_addr_lines;
-    struct gpiod_line *req_read_bus_line;
-    struct gpiod_line *reg_rdata_valid_line;
+    struct gpiod_line *req_bus_line;
+    struct gpiod_line *op_done_line;
+    struct gpiod_line *reg_wen_line;
+    struct gpiod_line *blk_wstart_line;
+    struct gpiod_line *blk_wen_line;
 };
 
 bool EMIO_Init(struct EMIO_Info *info)
 {
     unsigned int reg_data_offsets[32];
     unsigned int reg_addr_offsets[16];
-    unsigned int req_read_bus_offset;
-    unsigned int reg_rdata_valid_offset;
+    unsigned int req_bus_offset;
+    unsigned int op_done_offset;
+    unsigned int reg_wen_offset;
+    unsigned int blk_wstart_offset;
+    unsigned int blk_wen_offset;
     int reg_addr_values[16];
 
     size_t i;
@@ -372,8 +378,11 @@ bool EMIO_Init(struct EMIO_Info *info)
         reg_addr_offsets[i] = INDEX_EMIO_START+47-i;
         reg_addr_values[i] = 0;
     }
-    req_read_bus_offset = INDEX_EMIO_START+48;
-    reg_rdata_valid_offset = INDEX_EMIO_START+49;
+    req_bus_offset = INDEX_EMIO_START+48;
+    op_done_offset = INDEX_EMIO_START+49;
+    reg_wen_offset = INDEX_EMIO_START+50;
+    blk_wstart_offset = INDEX_EMIO_START+51;
+    blk_wen_offset = INDEX_EMIO_START+52;
 
     // First, open the chip
     info->chip = gpiod_chip_open("/dev/gpiochip0");
@@ -400,11 +409,20 @@ bool EMIO_Init(struct EMIO_Info *info)
     // Set all lines to output, initialized to 0
     gpiod_line_request_bulk_output(&info->reg_addr_lines, "fpgav3init", reg_addr_values);
 
-    info->req_read_bus_line = gpiod_chip_get_line(info->chip, req_read_bus_offset);
-    gpiod_line_request_output(info->req_read_bus_line, "fpgav3init", 0);
+    info->req_bus_line = gpiod_chip_get_line(info->chip, req_bus_offset);
+    gpiod_line_request_output(info->req_bus_line, "fpgav3init", 0);
 
-    info->reg_rdata_valid_line = gpiod_chip_get_line(info->chip, reg_rdata_valid_offset);
-    gpiod_line_request_input(info->reg_rdata_valid_line, "fpgav3init");
+    info->op_done_line = gpiod_chip_get_line(info->chip, op_done_offset);
+    gpiod_line_request_input(info->op_done_line, "fpgav3init");
+
+    info->reg_wen_line = gpiod_chip_get_line(info->chip, reg_wen_offset);
+    gpiod_line_request_output(info->reg_wen_line, "fpgav3init", 0);
+
+    info->blk_wstart_line = gpiod_chip_get_line(info->chip, blk_wstart_offset);
+    gpiod_line_request_output(info->blk_wstart_line, "fpgav3init", 0);
+
+    info->blk_wen_line = gpiod_chip_get_line(info->chip, blk_wen_offset);
+    gpiod_line_request_output(info->blk_wen_line, "fpgav3init", 0);
 
     return true;
 }
@@ -417,7 +435,7 @@ void EMIO_Release(struct EMIO_Info *info)
 }
 
 // EMIO_ReadQuadlet: read quadlet from specified address
-void EMIO_ReadQuadlet(struct EMIO_Info *info, uint16_t addr, uint32_t *data)
+bool EMIO_ReadQuadlet(struct EMIO_Info *info, uint16_t addr, uint32_t *data)
 {
     int reg_rdata_values[32];
     int reg_addr_values[16];
@@ -426,25 +444,73 @@ void EMIO_ReadQuadlet(struct EMIO_Info *info, uint16_t addr, uint32_t *data)
     for (i = 0; i < 16; i++)
         reg_addr_values[i] = (addr&(0x8000>>i)) ? 1 : 0;
 
+    // Set all data lines to input
+    if (gpiod_line_request_bulk_input(&info->reg_data_lines, "fpgav3init") != 0) {
+        printf("EMIO_ReadQuadlet: could not set data lines as input\n");
+        return false;
+    }
+
     // Write reg_addr (read address)
     gpiod_line_set_value_bulk(&info->reg_addr_lines, reg_addr_values);
-    // Set req_read_bus to 1 (rising edge requests firmware to read register)
-    gpiod_line_set_value(info->req_read_bus_line, 1);
-    // reg_rdata_valid should be set quickly by firmware, to indicate
+    // Set req_bus to 1 (rising edge requests firmware to read register)
+    gpiod_line_set_value(info->req_bus_line, 1);
+    // op_done should be set quickly by firmware, to indicate
     // that read has completed
-    if (gpiod_line_get_value(info->reg_rdata_valid_line) != 1) {
+    if (gpiod_line_get_value(info->op_done_line) != 1) {
         printf("Waiting to read");
         // Should set a timeout for following while loop
-        while (gpiod_line_get_value(info->reg_rdata_valid_line) != 1)
+        while (gpiod_line_get_value(info->op_done_line) != 1)
             printf(".");
         printf("\n");
     }
     // Read data from reg_data
     gpiod_line_get_value_bulk(&info->reg_data_lines, reg_rdata_values);
-    // Set req_read_bus to 0
-    gpiod_line_set_value(info->req_read_bus_line, 0);
+    // Set req_bus to 0
+    gpiod_line_set_value(info->req_bus_line, 0);
     for (i = 0; i < 32; i++)
         *data = (*data<<1)|reg_rdata_values[i];
+    return true;
+}
+
+// EMIO_WriteQuadlet: write quadlet to specified address
+bool EMIO_WriteQuadlet(struct EMIO_Info *info, uint16_t addr, uint32_t data)
+{
+    int reg_wdata_values[32];
+    int reg_addr_values[16];
+    size_t i;
+
+    for (i = 0; i < 16; i++)
+        reg_addr_values[i] = (addr&(0x8000>>i)) ? 1 : 0;
+
+    for (i = 0; i < 32; i++)
+        reg_wdata_values[i] = (data&(0x80000000>>i)) ? 1 : 0;
+
+    // Set all data lines to output and write values
+    if (gpiod_line_request_bulk_output(&info->reg_data_lines, "fpgav3init", reg_wdata_values) != 0) {
+        printf("EMIO_WriteQuadlet: could not set data lines as output\n");
+        return false;
+    }
+
+    // Write reg_addr (write address)
+    gpiod_line_set_value_bulk(&info->reg_addr_lines, reg_addr_values);
+
+    // Set reg_wen to 1
+    gpiod_line_set_value(info->reg_wen_line, 1);
+
+    // Set req_bus to 1 (rising edge requests firmware to write register)
+    gpiod_line_set_value(info->req_bus_line, 1);
+    // op_done should be set quickly by firmware, to indicate
+    // that write has completed
+    if (gpiod_line_get_value(info->op_done_line) != 1) {
+        printf("Waiting to write");
+        // Should set a timeout for following while loop
+        while (gpiod_line_get_value(info->op_done_line) != 1)
+            printf(".");
+        printf("\n");
+    }
+    // Set req_bus to 0
+    gpiod_line_set_value(info->req_bus_line, 0);
+    return true;
 }
 
 int main(int argc, char **argv)
--- platform_standalone/fsbl_src/fsbl_hooks.c
+++ platform_standalone/fsbl_src/fsbl_hooks.c
@@ -24,6 +24,7 @@
 *
 ******************************************************************************/
 
+#include <stdbool.h>
 #include "fsbl.h"
 #include "xstatus.h"
 #include "xil_printf.h"
@@ -50,55 +51,124 @@ enum EMIO_Reg {
 // Bit masks for upper EMIO bits
 enum EMIO_Bits {
     Bits_RegAddr        = 0x0000ffff,   // 16-bit address (output)
-    Bits_RequestReadBus = 0x00010000,   // Request read bus (output)
-    Bits_ReadDataValid  = 0x00020000    // Read data valid (input)
+    Bits_RequestBus     = 0x00010000,   // Request read or write bus (output)
+    Bits_OpDone         = 0x00020000,   // Read or write done (input)
+    Bits_RegWen         = 0x00040000,   // Register write enable (output)
+    Bits_BlkWStart      = 0x00080000,   // Block write start (output)
+    Bits_BlkWen         = 0x00100000,   // Block write enable (output)
+    Bits_Write          = 0x00200000    // Write operation detected (based on tristate)
 };
 
 void EMIO_Init()
 {
-    // emio[31:0] is reg_data (currently input only)
+    // emio[31:0] is reg_data (initialize as input)
     Xil_Out32(XPS_GPIO_BASEADDR + Reg_DirLower, 0x00000000);
-    // emio[49] is reg_rdata_valid (input)
-    // emio[48] is req_read_bus (output)
+    // emio[53] is write (input)
+    // emio[52] is blk_wen (output)
+    // emio[51] is blk_wstart (output)
+    // emio[50] is reg_wen (output)
+    // emio[49] is op_done (input)
+    // emio[48] is req_bus (output)
     // emio[47:32] is reg_addr (output)
     Xil_Out32(XPS_GPIO_BASEADDR + Reg_DirUpper,
-              Bits_RegAddr | Bits_RequestReadBus);
+              Bits_RegAddr | Bits_RequestBus | Bits_RegWen | Bits_BlkWStart | Bits_BlkWen);
     // Initialize all outputs to 0
     Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, 0x00000000);
     // Enable all outputs
     Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutEnUpper,
-              Bits_RegAddr | Bits_RequestReadBus);
+              Bits_RegAddr | Bits_RequestBus | Bits_RegWen | Bits_BlkWStart | Bits_BlkWen);
 }
 
-void EMIO_ReadQuadlet(uint16_t addr, uint32_t *data)
+bool EMIO_ReadQuadlet(uint16_t addr, uint32_t *data)
 {
+    // We assume the data lines (reg_data) are set for input, since that
+    // is their initial value and the Write methods set them to input before
+    // returning. But, the following test will verify this assumption.
+    uint32_t upper;
+    const uint32_t upper_mask = Bits_RequestBus | Bits_Write;
+    upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
+    // Do not want RequestBus or Write to be set
+    if (upper & upper_mask) {
+        xil_printf("EMIO_ReadQuadlet: invalid state %x\r\n", upper);
+        return false;
+    }
     uint32_t outreg = addr;
     // Write reg_addr (read address)
-    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, outreg);
-    // Set req_read_bus to 1 (rising edge requests firmware to read register)
-    outreg |= Bits_RequestReadBus;
-    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, outreg);
-    // reg_rdata_valid should be set quickly by firmware, to indicate that read
+    // Also set req_bus to 1 (rising edge requests firmware to read register)
+    // Because the firmware latches req_bus, there is enough delay that we can set it now
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, outreg | Bits_RequestBus);
+    // op_done should be set quickly by firmware, to indicate that read
     // has completed. If the FPGA bus is not busy (due to Firewire or Ethernet
     // access), it should only take about 1 iteration of the following loop.
     int i;
-    for (i = 0; i < 3; i++) {
-        if (Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper) & Bits_ReadDataValid) break;
+    upper = 0;
+    for (i = 0; (i < 5) && !(upper & Bits_OpDone); i++) {
+        upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
     }
-    if (i == 3) {
+    if (i == 5) {
         // If the above loop timed out, we print a message and keep checking.
         // It would be better to set a timeout to avoid an infinite loop.
         xil_printf("Waiting to read addr %x", addr);
-        while (!(Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper) & Bits_ReadDataValid)) {
+        while (!(upper & Bits_OpDone)) {
+            upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
             xil_printf(".");
-	    i++;
-	}
+            i++;
+        }
         xil_printf("\r\nWaited %d iterations\r\n", i);
     }
     // Read data from reg_data
     *data = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputLower);
-    // Set req_read_bus to 0 (also sets reg_addr to 0)
+    // Set req_bus to 0 (also sets reg_addr to 0)
     Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, 0x00000000);
+    return true;
+}
+
+bool EMIO_WriteQuadlet(uint16_t addr, uint32_t data)
+{
+    // Set reg_data as output
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_DirLower, 0xffffffff);
+    // Enable all outputs
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutEnLower, 0xffffffff);
+    // Now, do some sanity checking
+    uint32_t upper;
+    const uint32_t upper_mask = Bits_RequestBus | Bits_Write;
+    upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
+    // Do not want RequestBus to be set, but Write must be set
+    if ((upper & upper_mask) != Bits_Write) {
+        xil_printf("EMIO_WriteQuadlet: invalid state %x\r\n", upper);
+        return false;
+    }
+    // Write reg_wdata (write data)
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputLower, data);
+    // Write reg_waddr (write address) and reg_wen
+    // Also set req_bus to 1 (rising edge requests firmware to write register)
+    // Because the firmware latches req_bus, there is enough delay that we can set it now
+    uint32_t outreg = addr;
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, outreg | Bits_RegWen | Bits_RequestBus);
+    // op_done should be set quickly by firmware, to indicate that write
+    // has completed. If the FPGA bus is not busy (due to Firewire or Ethernet
+    // access), it should only take about 1 iteration of the following loop.
+    int i;
+    upper = 0;
+    for (i = 0; (i < 5) && !(upper & Bits_OpDone); i++) {
+        upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
+    }
+    if (i == 5) {
+        // If the above loop timed out, we print a message and keep checking.
+        // It would be better to set a timeout to avoid an infinite loop.
+        xil_printf("Waiting to write addr %x", addr);
+        while (!(upper & Bits_OpDone)) {
+            upper = Xil_In32(XPS_GPIO_BASEADDR + Reg_InputUpper);
+            xil_printf(".");
+            i++;
+        }
+        xil_printf("\r\nWaited %d iterations\r\n", i);
+    }
+    // Set req_bus to 0 (also sets reg_addr and reg_wen to 0)
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_OutputUpper, 0x00000000);
+    // Set reg_data as input (default state)
+    Xil_Out32(XPS_GPIO_BASEADDR + Reg_DirLower, 0x00000000);
+    return true;
 }
 
 /******************************************************************************
