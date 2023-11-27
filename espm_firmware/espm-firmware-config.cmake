7ff4b2bcc17398fb5fd0970b466e8e2ae7c13f8e
Peter Kazanzides <pkaz@.jhu.edu>
2023-11-26 22:35:41 -0500

    Updated ESPM firmware; set embedded version to 1.0.1

--- espm_firmware/espm-firmware-config.cmake
+++ espm_firmware/espm-firmware-config.cmake
@@ -2,7 +2,7 @@
 # CMake configuration file for espm-firmware
 #
 
-set (ESPM_FIRMWARE_VERSION 1)
+set (ESPM_FIRMWARE_VERSION 2)
 
 set (ESPM_FIRMWARE_NAME "espm.xsvf")
 set (ESPM_FIRMWARE_FILE "${CMAKE_CURRENT_LIST_DIR}/${ESPM_FIRMWARE_NAME}")
